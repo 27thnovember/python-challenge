9977ead1a1ce7852faecaeb3b903e164799c8825
Chidiebere joshua Nwobodo <117982112+27thnovember@users.noreply.github.com>
2023-10-15 07:54:18 +0100

    Add files via upload

---
+++ 100 days of coding with Python/Day 1 Task(100 Days of code ).py
@@ -0,0 +1,9 @@
+print("""
+Chidiebere
+october 15th 2023
+I am signing up fpr Replit's 100 days challenge!
+I will make sure to spend some time everday coding along, for a minimum of Ten minutes a Day,
+I'll be using Replit, an amazing youtubeChannel so i can do this with my Laptop. No excuses from the middle of love Garden!
+I am Feelimg (.-.)
+Follow my progress @ @27th November.github.com"""
+    )
---
+++ 100 days of coding with Python/Day 1 of 100 Days of coding Python.py
@@ -0,0 +1,10 @@
+print("Hello Replit")
+print("and how are you")
+
+print("FG GOod Morning")
+
+print("""
+    Nneka
+    Chineye
+    Nkerika
+""")
