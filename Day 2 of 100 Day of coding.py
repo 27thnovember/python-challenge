8deba36fae039ecc5d8b89ae199b691bf5154e4c
Chidiebere joshua Nwobodo <117982112+27thnovember@users.noreply.github.com>
2023-10-16 17:57:41 +0100

    Add files via upload

---
+++ Day 2 of 100 Day of coding.py
@@ -0,0 +1,16 @@
+myName = input("What is your name?: ")
+
+favouriteFood = input("What is your favourite Food? : ") 
+
+favouriteMusic = input("Whats your Favourite Music? : ")
+
+Residence = input("Where do you live? : ")
+
+print("You are")
+print(myName)
+print('You are probably Hungry for')
+print (favouriteFood)
+print ('you are definitely getting your your groove on to My Man'),
+print(favouriteMusic)
+print ('living in the amazing california')
+print(Residence)
