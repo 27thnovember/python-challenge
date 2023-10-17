5b09ffbe261ed14b5951d2995af4fdd6b90e088c
Chidiebere joshua Nwobodo <117982112+27thnovember@users.noreply.github.com>
2023-10-17 23:34:48 +0100

    Add files via upload

---
+++ Day 3.py
@@ -0,0 +1,8 @@
+NameOFfood = input('Name a Food: ')
+NameOfplant = input('Name a plant: ')
+methodOfcooking = input('Name a method of cooking: ')
+DescribingBurntfood = input('Name a Ruined Food: ')
+nameaDIYitem = input('Name a house Hold Item: ')
+
+print('MENU')
+print (methodOfcooking, NameOFfood, 'with', DescribingBurntfood, 'cactus on a bed of' , nameaDIYitem) 
