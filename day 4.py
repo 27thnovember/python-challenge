bac13dc9aca563c399038b3e83028d16c6f2f1d3
Chidiebere joshua Nwobodo <117982112+27thnovember@users.noreply.github.com>
2023-10-18 23:14:25 +0100

    Add files via upload

---
+++ day 4.py
@@ -0,0 +1,11 @@
+print(' Your Adventurer Simulator')
+print("""You'll be asked a couple of questions then we will make you a customized story with you as Star!""")
+print()
+name = input('Your name: ')
+enemy = input('Your worst enemys name: ')
+superPower = input('Your super power: ')
+print()
+print('our story begins as our hero', name, 'approaches a forbeding castle_')
+
+print('Suddenly a strong wind covers the whole castle and', enemy, 'comes foreth from the dust')
+print('"our Final battle begins!" shouts the evil', enemy, "clearly missing the fact that",name,"has the power",superPower,"which means they will win quite easily")
