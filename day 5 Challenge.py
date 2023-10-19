bf48a5425c461f1f79d05e73ebb537bf34c2e0d9
Chidiebere joshua Nwobodo <117982112+27thnovember@users.noreply.github.com>
2023-10-19 23:25:23 +0100

    Add files via upload

---
+++ day 5 Challenge.py
@@ -0,0 +1,16 @@
+#Manhwa Character Selcetion
+print('Find out Your Destined Manhwa Character')
+
+LikesDislike = input('If you where to win a lottery would you show off to your friends ')
+if LikesDislike == "Yes":
+    print('Your Manhwa Character is Yang Kai')
+else:
+    print('You are not Yang kai')
+Attributes = input('Do you have Bar Hair? ')
+if Attributes == "Yes":
+    print('You are Su ling')
+else:
+    print('You are not su ling')
+Stateofmind = input('Do want to make more Money? ')
+if Stateofmind == "Yes":
+    print('Aha! You are Lu shu')
---
+++ day 5.py
@@ -0,0 +1,14 @@
+#Testing If and else function#
+print('Testing if and else functions')
+myName = input("what is your name: ")
+if myName == 'Chika':
+    print('welcome Chika')
+    print('Your are the sugar in my Tea')
+else:
+    print('Who the fuck are you?!')
+Feeling = input('How are you feeling Today? ')
+if Feeling == 'Fine':
+    print("I am glad you are doing Great")
+else:
+    print('It will be okay my love')
+    
